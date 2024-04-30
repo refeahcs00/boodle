916c3c4f217fa8325a22823f1acb702ebea822dc
refeahcs00 <owens0506@gmail.com>
2024-04-29 21:18:52 -0400

    project save 1. entering back into project 2. maybe i will actively develop this

--- app/views.py
+++ app/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+VERSION = '0.0.1'
+BANNER_SLANT = '''
+    ____  ____  ____  ____  __    ______
+   / __ )/ __ \/ __ \/ __ \/ /   / ____/
+  / __  / / / / / / / / / / /   / __/   
+ / /_/ / /_/ / /_/ / /_/ / /___/ /___   
+/_____/\____/\____/_____/_____/_____/   
+                                        
+'''
+
 # Create your views here.
 def index(request): 
-    return HttpResponse("Hello World!")
+    # print(f'{BANNER_SLANT}\n{VERSION}\n')
+
+    return render(request, 'app/index.html', {})
